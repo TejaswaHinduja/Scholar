6aec56568081b5caf5cb93631c7a129d3d5eace7
Tejaswa <tejaswahinduja@gmail.com>
2026-06-21 12:57:07 +0530

    build forms

--- frontend/app/auth/[type]/page.tsx
+++ frontend/app/auth/[type]/page.tsx
@@ -1,15 +1,26 @@
+"use client"
 import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
 import {useParams,useRouter} from "next/navigation"
-
-
-
+import { useState } from "react";
+import {useForm,Controller} from "react-hook-form"
 
 export default function AuthPage(){
+    const form=useForm()
+    const {register,handleSubmit,formState:{errors},control}=form
     
-
+    const Submit=async()=>{
+        const res=await fetch(`http://localhost:5000/api/signup`,{
+            method:"POST",
+            headers:{"Content-Type":"application/json"},
+            body:JSON.stringify(values)
+        })
+    }
     return (
         <div>
+            <Input placeholder="Name" onChange={(e)=>{setName(e.target.value)}}/>
+            <Input placeholder="email" onChange={(e)=>{setEmail(e.target.value)}}/>
+            <Button type="submit" onClick={Submit}>Submit</Button>
 
         </div>
     )
