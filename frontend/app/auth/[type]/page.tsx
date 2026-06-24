be6c0bca562d6ea8c7216660b25355b948b51028
Tejaswa <tejaswahinduja@gmail.com>
2026-06-24 16:48:05 +0530

    setting up auth pages

--- frontend/app/auth/[type]/page.tsx
+++ frontend/app/auth/[type]/page.tsx
@@ -6,22 +6,36 @@ import { useState } from "react";
 import {useForm,Controller} from "react-hook-form"
 
 export default function AuthPage(){
-    const form=useForm()
+    type formvalue={
+        name:string,
+        email:string,
+        password:string
+    }
+    const {typeAuth}=useParams()
+    const form=useForm<formvalue>({
+        
+    })
     const {register,handleSubmit,formState:{errors},control}=form
     
-    const Submit=async()=>{
-        const res=await fetch(`http://localhost:5000/api/signup`,{
+    const Submit=async(values:formvalue)=>{
+        {/*const res=await fetch(`http://localhost:5000/api/signup`,{
             method:"POST",
             headers:{"Content-Type":"application/json"},
             body:JSON.stringify(values)
         })
+        console.log(res) */}
+        console.log(values)
     }
     return (
         <div>
-            <Input placeholder="Name" onChange={(e)=>{setName(e.target.value)}}/>
-            <Input placeholder="email" onChange={(e)=>{setEmail(e.target.value)}}/>
-            <Button type="submit" onClick={Submit}>Submit</Button>
-
+            <form className="flex flex-col w-full justify-center items-center"onSubmit={handleSubmit(Submit)}>
+            <div className="max-w-3xl my-4 gap-4">
+            <Input {...register("name")}placeholder="Name" />
+            <Input className="my-4" {...register("email")}placeholder="email" />
+            <Button className="align-center items-center"type="submit">Submit</Button>
+            </div>
+            </form>
+            
         </div>
     )
 }
