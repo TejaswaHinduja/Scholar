a24d28cc14b9b61ae2a2339ea13698868bec6da0
Tejaswa <tejaswahinduja@gmail.com>
2026-06-27 20:52:45 +0530

    protect middleware, feed route

--- backend/src/routes/feed.ts
+++ backend/src/routes/feed.ts
@@ -2,3 +2,21 @@ import { Router } from "express";
 import bcrypt from "bcrypt";
 import { genToken } from "../util/jwt.js";
 import {prisma} from "../db.js"
+import { Protect } from "../util/protect.js";
+const mainrouter=Router()
+
+mainrouter.get("/feed", Protect , async(req , res)=>{
+    const userId=req.body.userId
+    if(!userId){
+        return
+    }
+    const usr=await prisma.user.findUnique({
+        where:{
+            id:userId
+        },
+        omit:{
+            password:true
+        }
+    })
+
+})
--- backend/src/util/protect.ts
+++ backend/src/util/protect.ts
@@ -14,28 +14,29 @@ export interface AuthRequest extends Request {
 }
 export async function Protect(req:AuthRequest,res:Response,next:NextFunction){
     
-    try{const token=req.cookies.jwt;
-    if(!token){
-        return res.status(403).json({message:"not authenticated"})
-    }
-    const secret=process.env.JWT_SECRET as string
-    const check=jwt.verify(token,secret) as MyTokenPayload
-    if(!check || !check.userId){
-        return res.status(403).json({message:"login again"})
-    }
-    const user=await prisma.user.findUnique({
-        where:{
-            id:check.userId
-        },
-        omit:{
-            password:true
+    try{
+        const token=req.cookies.jwt;
+        if(!token){
+            return res.status(403).json({message:"not authenticated"})
         }
-    })
-    if(!user){
-        return res.status(500).json({message:"please sign up"})
-    }
-    req.user=user
-    next()
+        const secret=process.env.JWT_SECRET as string
+        const check=jwt.verify(token,secret) as MyTokenPayload
+        if(!check || !check.userId){
+            return res.status(403).json({message:"login again"})
+        }
+        const user=await prisma.user.findUnique({
+            where:{
+                id:check.userId
+            },
+            omit:{
+                password:true
+            }
+        })
+        if(!user){
+            return res.status(500).json({message:"please sign up"})
+        }
+        req.user=user
+        next()
 
 }
     catch(e){
